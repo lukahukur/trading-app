d74ee16e1fcf2ad0613b223c5afbdd5135949f2b
lukahukur <6wingman9@gmail.com>
2021-12-08 23:23:04 +0400

    lock.json;css-buyForm

--- src/components/buySellForm.jsx
+++ src/components/buySellForm.jsx
@@ -9,13 +9,13 @@ function BuySellForm({sell,isSelling}){
         <div className='gg'>
             
             <div className='btn-wp'>
-           <input type="button" value='Buy'/>
-           <input type="button" value='Sell'/>
+           <input type="button" value='BUY'/>
+        <input type="button" value='SELL'/>
            </div>
 
            <div className='inp_wp'>
-           <input type="text" placeholder='Price'/>
-           <input type="text" placeholder='Amount'/>
+         <div className='inp_wp_div'>  <p>Price</p><input type="text" /></div>
+         <div className='inp_wp_div'> <p>Amount</p> <input type="text"/></div>
            <input type="range" />
            <input type="button" value={sell === true ?'sell' : 'buy'}/>
            </div>
